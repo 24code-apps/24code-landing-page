5ef7fed6fef0eb49185d3c1e4ead871fe47c0d0d
benzigar-pepul <benzigar@pepul.com>
2023-05-15 21:43:10 +0530

    url fix

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || "https://24code.in",
+  siteUrl: process.env.SITE_URL || "https://www.24code.in",
   generateRobotsTxt: true, // (optional)
   // ...other options
 };
