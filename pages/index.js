5ff9f8cbb806e7b32da8b755dcd466bf6c2046e7
Benzigar Codes <benzigar619@gmail.com>
2023-05-19 11:43:56 +0530

    added tech company

--- pages/index.js
+++ pages/index.js
@@ -11,10 +11,38 @@ import {
 import Seo from "../components/Seo";
 
 const App = () => {
+  const techs = [
+    {
+      name: "Google Cloud",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/2008px-Google_%22G%22_Logo.svg.png",
+    },
+    {
+      name: "Vercel",
+      image:
+        "https://static-00.iconduck.com/assets.00/vercel-icon-512x449-3422jidz.png",
+    },
+    {
+      name: "Digital Ocean",
+      image:
+        "https://companieslogo.com/img/orig/DOCN-6eec72eb.png?t=1660638083",
+    },
+    {
+      name: "AWS",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/Amazon_Web_Services_Logo.svg/1280px-Amazon_Web_Services_Logo.svg.png",
+    },
+    {
+      name: "Planet Scale",
+      image:
+        "https://seeklogo.com/images/P/planetscale-logo-0EEA8CAEB4-seeklogo.com.png",
+    },
+  ];
+
   return (
     <div className="min-h-[70vh] bg-white flex flex-col">
       <Seo />
-      <div className="flex-1 bg-white text-black justify-center items-center flex flex-col">
+      <div className="mt-10 flex-1 bg-white text-black justify-center items-center flex flex-col">
         {/* <h1 className="px-10 text-center text-4xl md:text-6xl lg:text-7xl max-w-[800px]">
           <span className="items-center flex flex-wrap">
             <img
@@ -59,11 +87,48 @@ const App = () => {
             className="hover:bg-zinc-100 border-2 text-black px-6 text-black py-3 rounded-md mt-5"
             href="https://forms.gle/tNqYzXqhQaZAdsmN7"
           >
-            Contact Us Now
+            Get Started
           </a>
         </div>
       </div>
-      <div className="mt-10 border-b-2 pb-5 flex flex-col items-center mx-auto">
+      <div className="mt-24 justify-center flex-col flex items-center">
+        <h2 className="text-[#0173C6] text-md">OUR TECH PARTNERS</h2>
+        <div className="mt-5 flex items-center">
+          {techs?.map((each) => (
+            <Link
+              target={"_blank"}
+              href={"https://www.google.com/search?q=" + each.name}
+            >
+              <img
+                className="grayscale h-10 mx-5"
+                src={each.image}
+                alt={each.name}
+              />
+            </Link>
+          ))}
+          {/* <img
+            className="h-10"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/Vercel_logo_black.svg/2560px-Vercel_logo_black.svg.png"
+            alt=""
+          />
+          <img
+            className="h-10"
+            src="https://consent.trustarc.com/v2/asset/20:55:23.714y33gof_DO_Logo-Blue.png"
+            alt=""
+          />
+          <img
+            className="h-10"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/2560px-Amazon_logo.svg.png"
+            alt=""
+          />
+          <img
+            className="h-10"
+            src="https://mms.businesswire.com/media/20220524005472/en/1465062/5/planetscale-logo-full-black.jpg"
+            alt=""
+          /> */}
+        </div>
+      </div>
+      <div className="mt-24 border-b-2 pb-5 flex flex-col items-center mx-auto">
         <p className="text-lg mb-1">
           Some of our Tiny <span className="font-bold">Apps</span> ❤️
         </p>
